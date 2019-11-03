54907bd984768f293e899888d1630cad129fca94
Wojciech Rak <rak.wojtek@wp.pl>
2019-11-03 23:40:35 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
